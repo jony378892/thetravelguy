e475e3e2e6e68923f27adfb0f441e8aba8857d4a
Jony <jony378892@gmail.com>
2025-08-04 22:20:09 +0600

    Changed cors middleware

--- server/app.js
+++ server/app.js
@@ -10,7 +10,12 @@ const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
 
 connectDB();
 
